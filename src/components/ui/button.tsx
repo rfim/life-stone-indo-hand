4b3798f85686056ef6d35b282d81ebd9f17267bd
R. Firman Insan M <firman.insan.m@gmail.com>
2025-08-24 14:45:09 +0000

    Generated by Spark: is not working

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -50,6 +50,7 @@ function Button({
   return (
     <Comp
       data-slot="button"
+      type={asChild ? undefined : "button"}
       className={cn(buttonVariants({ variant, size, className }))}
       {...props}
     />
